ea4c8a5d6358f8b5c94c52b6e25d84d50b24e190
Achal Mukaty <yashmukaty1@gmail.com>
2021-06-17 19:52:53 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 import ReactDOM from "react-dom"
+import { BrowserRouter } from "react-router-dom"
 //component file
-import TodoContainer from "./components/TodoContainer"
+import TodoContainer from "./FunctionBased/components/TodoContainer"
 
 //stylesheet
-import "./App.css"
+import "./FunctionBased/App.css"
 
 ReactDOM.render(
   <React.StrictMode>
+    <BrowserRouter>
     <TodoContainer />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
 );
